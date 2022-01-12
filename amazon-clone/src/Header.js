11f416e9488ba07cc0f6e3f30032ca7ee425d808
bbouya <52068209+bbouya@users.noreply.github.com>
2022-01-13 00:41:06 +0100

    add

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from "@material-ui/icons/Search";
-import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+//import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 function Header() {
     return (
         <div className='header'>
             <img className='header__logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'></img>
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
-                <SearchIcon className="header__searchIcon">
-                    
-                </SearchIcon>
+                <SearchIcon className="header__searchIcon"/>
             </div>
             <div className='header_nav'>
                 <div className='header_option'>
